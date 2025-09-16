2b1f17608a971dbe74e4346613d8167059d31a57
Muhammad Hamza <codingtimelapseyt@gmail.com>
2025-09-16 15:30:24 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# site
+
+budgie landing page.
---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Work_Sans } from "next/font/google";
+import "./globals.css";
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "budgie",
+  description: "budgie makes software to help.",
+};
+
+export default function RootLayout({ children }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${workSans.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
