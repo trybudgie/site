59c519230a570f0f2fe87e4a0e40d0292a1c48a3
Muhammad Hamza <codingtimelapseyt@gmail.com>
2025-09-17 11:49:29 -0400

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,32 @@ const workSans = Work_Sans({
 export const metadata: Metadata = {
   title: "budgie",
   description: "making a flock of software.",
+  openGraph: {
+    title: "budgie",
+    description: "making a flock of software.",
+    images: [
+      {
+        url: "/budgie.png",
+        width: 500,
+        height: 500,
+        alt: "budgie",
+        type: "image/png",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "budgie",
+    description: "making a flock of software.",
+    images: ["/budgie.png"],
+  },
+  icons: {
+    icon: "/budgie.png",
+    shortcut: "/budgie.png",
+    apple: "/budgie.png",
+  },
 };
 
 export default function RootLayout({
